4fde7362d0fb02d3dcd992cae8034060e6094b30
clark159 <clark159@hotmail.com>
2023-12-20 06:54:26 +0800

    [src] MDP.AspNetCore.AdminLTE: Update Library

--- src/MDP.AspNetCore.AdminLTE/gulpfile.js
+++ src/MDP.AspNetCore.AdminLTE/gulpfile.js
@@ -1,8 +1,8 @@
 // variables
 var libraryList = [
-    { name: "admin-lte", path: "./node_modules/admin-lte/dist/**/", src: ["*.css*", "*.js*", "*.png", "*.jpg"] },
-    { name: "bootstrap", path: "./node_modules/bootstrap/dist/**/", src: ["*.css*", "*.js*"] },
     { name: "jquery", path: "./node_modules/jquery/dist/**/", src: ["*.js*"] },
+    { name: "bootstrap", path: "./node_modules/bootstrap/dist/**/", src: ["*.css*", "*.js*"] },
+    { name: "admin-lte", path: "./node_modules/admin-lte/dist/**/", src: ["*.css*", "*.js*", "*.png", "*.jpg"] },
     { name: "fontawesome-free", path: "./node_modules/@fortawesome/fontawesome-free/**/", src: ["*.css*", "*.js*"] },
     { name: "fontawesome-free", path: "./node_modules/@fortawesome/fontawesome-free/*webfonts*/", src: ["*"] }
 ];
@@ -13,7 +13,10 @@ var rimraf = require("rimraf");
 
 // task
 gulp.task("lib-clean", function (cb) {
-    rimraf("./wwwroot/lib/", cb);
+    rimraf("./wwwroot/lib/jquery", cb);
+    rimraf("./wwwroot/lib/bootstrap", cb);
+    rimraf("./wwwroot/lib/admin-lte", cb);
+    rimraf("./wwwroot/lib/fontawesome-free", cb);
 });
 
 gulp.task("lib-copy", function (done) {
